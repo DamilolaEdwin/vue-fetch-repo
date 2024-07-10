a1ac95eda5f5554ffa63935504faf501bb39c975
DamilolaEdwin <philipedwin035@gmail.com>
2024-07-10 15:26:18 +0100

    building up my functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <script setup>
+import {reactive, onMounted} from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+const state = reactive({
+    userData: null,
+    repos: [],
+    currentPage: 1,
+    perPage: 4,
+    totalPages: 1
+});
+
+const fetchUserData = () => {
+    fetch(`https://api.github.com/users/DamilolaEdwin`)
+        .then((res) => res.json())
+        .then((data) => {
+            state.userData = data;
+        });
+};
+
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -13,6 +31,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+       
       </nav>
     </div>
   </header>
